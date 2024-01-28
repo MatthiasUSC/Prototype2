07903e1b83d3b308e4b3b109244bbe5aa0094bf8
Matthias Horgen <matthiaskrh123@gmail.com>
2024-01-27 16:14:05 -0800

    bouncy

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -27,35 +27,38 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         // Ground check
-        RaycastHit2D[] hits;
-        hits = Physics2D.LinecastAll(groundCheck1.transform.position, groundCheck2.transform.position);
         isGrounded = false;
-        for (int i = 0; i < hits.Length; i++){
-            RaycastHit2D hit = hits[i];
-            if(hit.transform.tag == "Ground"){
+        RaycastHit hit;
+        if(Physics.Raycast(groundCheck1.transform.position, Vector3.down, out hit, 0.1f)){
+            if(hit.transform.tag == "Ground" || hit.transform.tag == "Player"){
+                isGrounded = true;
+            }
+        }
+        if(Physics.Raycast(groundCheck2.transform.position, Vector3.down, out hit, 0.1f)){
+            if(hit.transform.tag == "Ground" || hit.transform.tag == "Player"){
                 isGrounded = true;
             }
         }
 
         // Controls
         if(isGrounded){
-            float horizontalVel = GetComponent<Rigidbody2D>().velocity.x;
-            float horizontalForce = horizontalAccel * GetComponent<Rigidbody2D>().mass;
+            float horizontalVel = GetComponent<Rigidbody>().velocity.x;
+            float horizontalForce = horizontalAccel * GetComponent<Rigidbody>().mass;
             if(Input.GetKey(left)){
                 if(horizontalVel >= -maxHorizontalSpeed){
-                    GetComponent<Rigidbody2D>().AddForce(Vector2.left * horizontalForce);
+                    GetComponent<Rigidbody>().AddForce(Vector3.left * horizontalForce);
                 }
             }
 
             if(Input.GetKey(right)){
                 if(horizontalVel <= maxHorizontalSpeed){
-                    GetComponent<Rigidbody2D>().AddForce(Vector2.right * horizontalForce);
+                    GetComponent<Rigidbody>().AddForce(Vector3.right * horizontalForce);
                 }
             }
         }
 
         if(Input.GetKey(jump) && isGrounded){
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpVelocity);
+            GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, jumpVelocity, 0);
         }
     }
 }
